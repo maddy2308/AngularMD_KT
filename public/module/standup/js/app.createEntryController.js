adfe77bd1eb04339904caa84b0ca95d955188294
maddy2308 <madhur.eng@gmail.com>
2016-07-16 19:13:59 -0700

    Created an Entry form and hooked it up with Angular to show the list as it gets added

--- public/module/standup/js/app.createEntryController.js
+++ public/module/standup/js/app.createEntryController.js
@@ -11,9 +11,28 @@
         var vm = this;
 
         vm.submitEntry = submitEntry;
+        vm.addForm = addForm;
+        vm.removeForm = removeForm;
+
+        vm.entries = [];
 
         function submitEntry () {
             alert("Submit clicked");
+        }
+
+        function addForm() {
+            var entryObject = {
+                'date' : vm.entry.date,
+                'plan' : vm.entry.plan
+            };
+            vm.entries.push(entryObject);
+            vm.entry.plan = '';
+        }
+
+        function removeForm(index) {
+            if (index >= 0) {
+                vm.entries.splice(index, 1);
+            }
         }
     }
 
